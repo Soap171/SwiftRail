66cfbeaa21204ce33083a923fd11b5bb60e18315
Soap121 <sachinthana621@gmail.com>
2023-10-10 12:13:44 +0530

    add

---
+++ web/src/routes/ParcelBooking.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ParcelBooking() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ParcelBooking
---
+++ web/src/routes/Subscriptions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Subscriptions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Subscriptions
