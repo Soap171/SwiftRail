fe0242e189a04fc848c3def7f9c6e8c8108d8e54
Soap171 <spiyumal48@gmail.com>
2023-10-27 15:28:44 +0530

    fixed

--- web/src/components/AuthContext.js
+++ web/src/components/AuthContext.js
@@ -8,17 +8,20 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userData, setUserData] = useState(null); // Add a state variable to store user data
 
   const login = (userData) => {
     setIsAuthenticated(true);
+    setUserData(userData); // Store the user data in the context
   };
 
   const logout = () => {
     setIsAuthenticated(false);
+    setUserData(null); // Clear user data on logout
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, userData, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- web/src/components/UserDetails.js
+++ web/src/components/UserDetails.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useAuth } from '../components/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Userdetails = () => {
+    const { userData, logout } = useAuth();
+    const navigate = useNavigate();
+
+    if (!userData) {
+        return <div>Loading...</div>;
+    }
+
+    const handleLogout = () => {
+        logout(); // Call the logout function from the context
+        navigate('/'); // Redirect to the home page after logout
+    };
+
     return (
         <div className="container light-style flex-grow-1 container-p-y">
             <h4 className="font-weight-bold py-3 mb-4">
-                Hello, User
+                Hello, {userData.userName}
             </h4>
             <div className="card overflow-hidden">
                 <div className="row no-gutters row-bordered row-border-light">
                     <div className="col-md-3 pt-0">
                         <div className="list-group list-group-flush account-settings-links">
                             <a className="list-group-item list-group-item-action show active" data-bs-toggle="list" href="#general">General</a>
-                            <a className="list-group-item list-group-item-action" data-bs-toggle="list" href="#change-password">Change password</a>
-                            <a className="list-group-item list-group-item-action" data-bs-toggle="list" href="#parcel-details">Parcel Details</a>
-                            <a className="list-group-item list-group-item-action" data-bs-toggle="list" href="#subscription">Subscription</a>
                         </div>
                     </div>
                     <div className="col-md-9 mt-4 mb-4">
@@ -22,72 +33,26 @@ const Userdetails = () => {
                                 <form className="row g-3 p-3">
                                     <div className="col-md-6">
                                         <label className="form-label">Username</label>
-                                        <input type="text" className="form-control" value="JohnDoe" readOnly />
+                                        <input type="text" className="form-control" value={userData.userName} readOnly />
                                     </div>
                                     <div className="col-md-6">
                                         <label className="form-label">Contact Number</label>
-                                        <input type="text" className="form-control" value="123-456-7890" />
+                                        <input type="text" className="form-control" value={userData.contactNo} />
                                     </div>
                                     <div className="col-md-12">
                                         <label className="form-label">Address</label>
-                                        <input type="text" className="form-control" value="123 Main St, City" />
+                                        <input type="text" className="form-control" value={userData.address} />
                                     </div>
                                     <div className="col-md-6">
                                         <label className="form-label">NIC</label>
-                                        <input type="text" className="form-control" value="123456789" />
+                                        <input type="text" className="form-control" value={userData.NIC} />
                                     </div>
                                     <div className="col-md-6">
                                         <label className="form-label">Email</label>
-                                        <input type="email" className="form-control" value="johndoe@example.com" />
+                                        <input type="email" className="form-control" value={userData.email} />
                                     </div>
-                                    <button className='btn  btn-primary'>Update</button>
+                                    <button className='btn btn-primary' onClick={handleLogout}>Log out</button>
                                 </form>
-                            </div>
-                            <div className="tab-pane fade" id="change-password">
-                                <form className="row g-3 p-3">
-                                    <div className="col-md-6">
-                                        <label className="form-label">Old Password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <label className="form-label">New Password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <label className="form-label">Confirm New Password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                    <button className='btn  btn-primary'>Update</button>
-                                </form>
-                            </div>
-                            <div className="tab-pane fade" id="parcel-details">
-                                <div className="row g-3 p-3">
-                                    <div className="col-12">
-                                        <label className="form-label">Parcel Tracking Number</label>
-                                        <input type="text" className="form-control" />
-                                    </div>
-                                    <div className="col-12">
-                                        <label className="form-label">Parcel Status</label>
-                                        <input type="text" className="form-control" readOnly value="In Transit" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="subscription">
-                                <div className="row g-3 p-3">
-                                    <div className="col-12">
-                                        <label className="form-label">Subscription Plan</label>
-                                        <input type="text" className="form-control" value="Premium Plan" readOnly />
-                                    </div>
-                                    <div className="col-12">
-                                        <label className="form-label">Available Amount</label>
-                                        <input type="text" className="form-control" value="$50.00" readOnly />
-                                    </div>
-                                    <div className="col-12">
-                                        <label className="form-label">Duration</label>
-                                        <input type="text" className="form-control" value="1 Year" readOnly />
-                                    </div>
-                                    <button className='btn  btn-primary'>Renew Subscription</button>
-                                </div>
                             </div>
                         </div>
                     </div>
