5e6fc33cb5210efe8fa455388b0ace68d600e6c3
Soap171 <spiyumal48@gmail.com>
2023-10-28 07:53:54 +0530

    add search schedules

--- web/src/routes/Schedules.js
+++ web/src/routes/Schedules.js
@@ -1,105 +1,70 @@
-import '../components/Button.css';
-import Footer from '../components/Footer';
-import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
-import ScheduleImg from '../assets/Schedule.jpg';
-import Select from 'react-select';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import React, { useState, useEffect } from 'react';
+import supabase from '../config/supabaseClient';
+import Select from 'react-select';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import ScheduleImg from '../assets/Schedule.jpg';
 
 function Schedules({ isAuthenticated }) {
   const [currentStation, setCurrentStation] = useState(null);
   const [destinationStation, setDestinationStation] = useState(null);
   const [schedules, setSchedules] = useState([]);
-  const [notificationPhoneNumber, setNotificationPhoneNumber] = useState('');
   const [validationError, setValidationError] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState('');
+  const [currentStationOptions, setCurrentStationOptions] = useState([]);
+  const [destinationStationOptions, setDestinationStationOptions] = useState([]);
 
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
-
-  // Function to hide the message after a delay
   useEffect(() => {
-    if (notificationMessage) {
-      const timer = setTimeout(() => {
-        setNotificationMessage('');
-        closeModal();
-      }, 5000); // Adjust the time (in milliseconds) as needed
-      return () => clearTimeout(timer);
-    }
-  }, [notificationMessage]);
+    async function fetchStations() {
+      try {
+        const { data: stationData, error: stationError } = await supabase
+          .from('railwayStation')
+          .select('stationId, city');
 
-  const handleSearch = () => {
+        if (stationError) {
+          console.error('Error fetching railway stations:', stationError);
+        } else {
+          const stationOptions = stationData.map(station => ({
+            value: station.stationId,
+            label: station.city,
+          }));
+          setCurrentStationOptions(stationOptions);
+          setDestinationStationOptions(stationOptions);
+        }
+      } catch (error) {
+        console.error('Error in fetching railway stations:', error);
+      }
+    }
+
+    fetchStations();
+  }, []);
+
+  const handleSearch = async () => {
     if (!currentStation || !destinationStation) {
-      setValidationError('Both current and destination stations are required.');
+      setValidationError('Please select both Current and Destination Stations.');
       return;
     }
-
+  
     setValidationError('');
-
-    // Implement the search logic here and update the schedules state
-    // For example:
-    // const foundSchedules = ... // Fetch schedules from the backend
-    // setSchedules(foundSchedules);
-    setSchedules(dummySchedules);
-  };
-
-  const handleNotify = (scheduleId) => {
-    if (isAuthenticated == false) {
-      // User is not authenticated, handle the unauthenticated case (e.g., show an error message or redirect to login).
-      setValidationError('You must be logged in to use the Notify feature.');
-    } else {
-      // User is authenticated, continue with the notification logic.
-      openModal();
-      // Additional logic to handle notifications here
+  
+    try {
+      const { data: scheduleData, error: scheduleError } = await supabase
+        .from('schedule')
+        .select('*')
+        .eq('stationId', currentStation.value)
+        .ilike('finalDestination', destinationStation.label); // Using ilike for case-insensitive search
+  
+      if (scheduleError) {
+        console.error('Error fetching schedules:', scheduleError);
+      } else {
+        setSchedules(scheduleData);
+      }
+    } catch (error) {
+      console.error('Error in fetching schedules:', error);
     }
   };
 
-  const handleNotification = () => {
-    if (!validatePhoneNumber(notificationPhoneNumber)) {
-      setValidationError('Please enter a valid phone number.');
-      return;
-    }
-
-    setValidationError('');
-
-    // Implement the logic to send notifications using notificationPhoneNumber
-    setNotificationMessage('You will receive an SMS shortly on departure.');
-  };
-
-  const validatePhoneNumber = (phoneNumber) => {
-    const phonePattern = /^[0-9]{10}$/; // Modify the pattern according to your requirements
-    return phonePattern.test(phoneNumber);
-  };
-
-
-
-  const dummySchedules = [
-    {
-      id: 1,
-      trainNumber: 'ABC123',
-      departureTime: '09:00 AM',
-      arrivalTime: '11:30 AM',
-    },
-    {
-      id: 2,
-      trainNumber: 'XYZ456',
-      departureTime: '12:30 PM',
-      arrivalTime: '03:00 PM',
-    },
-    // Add more dummy schedule objects here
-  ];
-
-  const stationOptions = [
-    { value: 'station1', label: 'Station 1' },
-    { value: 'station2', label: 'Station 2' },
-    { value: 'station3', label: 'Station 3' },
-    // Add more station options here
-  ];
   return (
-   
     <div>
       <Navbar />
       <Hero cName="hero-other" heroImg={ScheduleImg} title="Easy Find Train Schedules" />
@@ -109,7 +74,7 @@ function Schedules({ isAuthenticated }) {
             <div className="form-group">
               <label>Current Station:</label>
               <Select
-                options={stationOptions}
+                options={currentStationOptions}
                 value={currentStation}
                 onChange={(selectedOption) => setCurrentStation(selectedOption)}
                 placeholder="Select current station"
@@ -120,7 +85,7 @@ function Schedules({ isAuthenticated }) {
             <div className="form-group">
               <label>Destination Station:</label>
               <Select
-                options={stationOptions}
+                options={destinationStationOptions}
                 value={destinationStation}
                 onChange={(selectedOption) => setDestinationStation(selectedOption)}
                 placeholder="Select destination station"
@@ -135,77 +100,34 @@ function Schedules({ isAuthenticated }) {
             </button>
           </div>
         </div>
-        {validationError && (
-          <div className="alert alert-danger mt-3">{validationError}</div>
-        )}
-      </div>
 
-      <div className="container mt-4">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Train Number</th>
-              <th>Departure Time</th>
-              <th>Arrival Time</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedules.map((schedule) => (
-              <tr key={schedule.id}>
-                <td>{schedule.trainNumber}</td>
-                <td>{schedule.departureTime}</td>
-                <td>{schedule.arrivalTime}</td>
-                <td>
-                  <button
-                    className="btn btn-success"
-                    onClick={() => handleNotify(schedule.id)}
-                  >
-                    Notify
-                  </button>
-                </td>
+        <div className="mt-4">
+          <h2>Fetched Schedules</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Train ID</th>
+                <th>Departure</th>
+                <th>Arrival</th>
+                {/* Add more table headers based on your schedule data */}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {schedules.map(schedule => (
+                <tr key={schedule.id}>
+                  <td>{schedule.trainId}</td>
+                  <td>{schedule.departureTime}</td>
+                  <td>{schedule.arrivalTime}</td>
+                  {/* Add more table cells based on your schedule data */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-
-      <Modal show={showModal} onHide={closeModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter Phone Number</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="form-group">
-            <label>Phone Number for Notification:</label>
-            <input
-              type="text"
-              className="form-control"
-              value={notificationPhoneNumber}
-              onChange={(e) => setNotificationPhoneNumber(e.target.value)}
-              placeholder="Enter phone number"
-            />
-          </div>
-          {notificationMessage && (
-            <div className="alert alert-success mt-3">{notificationMessage}</div>
-          )}
-          {validationError && (
-            <div className="alert alert-danger mt-3">{validationError}</div>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleNotification}>
-            Notify
-          </Button>
-        </Modal.Footer>
-      </Modal>
       <Footer />
     </div>
   );
 }
 
 export default Schedules;
-
-          
