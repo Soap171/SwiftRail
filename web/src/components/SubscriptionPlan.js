2494d7fde81c45ac0e2ba4e55aea45a9450071f9
Soap171 <spiyumal48@gmail.com>
2023-11-07 07:42:39 +0530

    fixed

--- web/src/components/SubscriptionPlan.js
+++ web/src/components/SubscriptionPlan.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthContext'; 
+import supabase from '../config/supabaseClient';
 
 function SubscriptionPlan(props) {
   const { title, price, description, primaryKey } = props;
   const navigate = useNavigate();
-  console.log('Received primaryKey:', primaryKey);
+  const { userData } = useAuth();
+  const [hasSubscription, setHasSubscription] = useState(false);
+
+  useEffect(() => {
+    const checkSubscription = async () => {
+      if (!userData || !userData.NIC) {
+        console.error('User data or NIC is missing or undefined.');
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from('customerSubscription')
+        .select('subscriptionId')
+        .eq('customerNIC', userData.NIC);
+
+      if (error) {
+        console.error('Error fetching subscription details:', error);
+      } else if (data && data.length > 0) {
+        setHasSubscription(true);
+      }
+    };
+
+    checkSubscription();
+  }, [userData]);
 
   const handleSubscribeClick = () => {
-    if (primaryKey) {
+    if (!hasSubscription && primaryKey) {
       navigate('/CheckoutPopup', { state: { subscriptionKey: primaryKey } });
+    } else if (hasSubscription) {
+      alert('User already has a subscription.Wait until it gets expired');
+      // Optionally, you could display a message or handle this case differently
     } else {
-      console.error('primaryKey is null or undefined.');
+      console.error('Invalid primaryKey or user data.');
     }
   };
 
